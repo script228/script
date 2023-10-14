edc509095fa69807bc8b2cb7fb9ddc38daa89ae1
script228 <147950544+script228@users.noreply.github.com>
2023-10-15 00:15:51 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options)
+driver.get("https://достижения.рф/achievements/region/601")
+t = 0
+while True:
+    xpath = '//*[@id="__nuxt"]/div/main/div/div[2]/div[3]/div[1]/div[2]/div[2]/button'
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
+    # driver.delete_all_cookies()
+    driver.execute_script('localStorage.clear();')
+    print(t)
+    t += 1
